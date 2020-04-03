0232b17000a58d955d2144f8c35ae565e1655811
bublinec <jurajbublinec@gmail.com>
2020-04-03 16:13:28 +0200

    Create index template

--- README.md
+++ README.md
@@ -9,26 +9,22 @@ MongoDB, Express, Node, Semantic UI
 ## Workflow:
 
 1. SETUP
-    * create package.json containing  metadata (especially dependencies using npm init command
-    * install necessary node packages
+    * create package.json containing  metadata (especially dependencies)using npm init command
+    * install necessary node packages using npm (node package manager)
         - express - node framework
         - mongoose - js interface for interacting with mongodb
         - ejs - embedded js - dynamic templates
         - body-parser - to get our data from a form
-    install using npm (node package manager)
-    command: npm i express ejs mongoose body-parser --save 
-    (also include in dependencies in package.json)
-
-    * create index.js (the main file) 
+    * create app.js (the main file) 
     * include packgaes using require
-    * app config
+    * configure app
     * configure mongo database using mongoose
     * setup server
 
 2. MONGOOSE SCHEMA + MODEL CONFIG
 3. RESTful ROUTES
 
-    * index - /blogs
+    * index (/blogs)
 
 
 
--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser"),
       express = require("express"),
       app = express();
 
-// * configure express
+// * configure app
 // set view engine (so we don't have to include .ejs all the time)
 app.set("view engine", "ejs");
 // inlcude public dir in express
@@ -19,10 +19,10 @@ mongoose.connect("mongodb://localhost/blog", {useNewUrlParser: true,useUnifiedTo
 const port = 8000;
 app.listen(port, function(err){
     if(err){
-        console.log("Something went wrong: ", err);
+        console.log("\nSomething went wrong: \n", err);
     }
     else{
-        console.log("Server listening on the port: ", port);
+        console.log("\nServer listening on the port: \n", port);
         
     }
 });
@@ -33,13 +33,27 @@ var blogSchema = new mongoose.Schema({
     title: String,
     body: String,
     date: {type: Date, default: Date.now},
-    img_url: {type: String, default: "public/img/default_img.png"}
+    img_url: {type: String, default: "img/default_img.png"}
 });
 var Blog = mongoose.model("Blog", blogSchema);
 
 
 // RESTful ROUTES
-// * index - /blogs
+// * index (/blogs)
+// redirect home page to index
+app.get("/", function(req, res){
+    res.redirect("/blogs")
+})
+
 app.get("/blogs", function(req, res){
-    res.render("index");
+    // get all the blogs from db
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log("\nSomething went wrong: \n", err);
+        }
+        else{
+            // render blogs
+            res.render("index", {blogs: blogs});
+        }
+    })
 });
